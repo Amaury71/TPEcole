f6800923720f69f7e5e82c8b0ec26c1d38d108d9
Antoine Plateaux <antoine.plateaux@sysclic.com>
2020-04-10 11:19:29 +0200

    Modification design

--- src/Migrations/Version20200409125321.php
+++ src/Migrations/Version20200409125321.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200402193312 extends AbstractMigration
+final class Version20200409125321 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,12 +23,13 @@ final class Version20200402193312 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content VARCHAR(255) DEFAULT NULL, date_add DATETIME NOT NULL, file_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE child (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, last_name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, age VARCHAR(255) NOT NULL, date_add DATETIME NOT NULL, class VARCHAR(255) NOT NULL, INDEX IDX_22B35429A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, send_msg_id INT NOT NULL, receive_msg_id INT DEFAULT NULL, msg VARCHAR(255) NOT NULL, date_envoie DATETIME NOT NULL, INDEX IDX_B6BD307F1BFD7200 (send_msg_id), INDEX IDX_B6BD307F611D1487 (receive_msg_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE school (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, adrs VARCHAR(255) NOT NULL, tel VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', password VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, tel VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE child ADD CONSTRAINT FK_22B35429A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F1BFD7200 FOREIGN KEY (send_msg_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F611D1487 FOREIGN KEY (receive_msg_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE child ADD CONSTRAINT FK_22B35429A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema) : void
@@ -40,6 +41,7 @@ final class Version20200402193312 extends AbstractMigration
         $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F1BFD7200');
         $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F611D1487');
         $this->addSql('DROP TABLE article');
+        $this->addSql('DROP TABLE child');
         $this->addSql('DROP TABLE message');
         $this->addSql('DROP TABLE school');
         $this->addSql('DROP TABLE user');
